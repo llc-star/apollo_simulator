c8db4e36602d6ea17f7e491f033561af5b46eff4
llc-star <79831185+llc-star@users.noreply.github.com>
2023-04-07 21:16:43 +0800

    Update lat_controller.cc

--- modules/control/controller/lat_controller.cc
+++ modules/control/controller/lat_controller.cc
@@ -210,16 +210,16 @@ Status LatController::Init(std::shared_ptr<DependencyInjector> injector,
    0.0, ((lr * cr - lf * cf) / i_z) / v, (l_f * c_f - l_r * c_r) / i_z,
    (-1.0 * (l_f^2 * c_f + l_r^2 * c_r) / i_z) / v;]
   */
-  matrix_a_(0, 1) = 0910-question;
-  matrix_a_(1, 2) = 0910-question;
-  matrix_a_(2, 3) = 0910-question;
-  matrix_a_(3, 2) = 0910-question;
+  matrix_a_(0, 1) = 1.0;
+  matrix_a_(1, 2) = (2cf_ + 2cr_) / mass_;
+  matrix_a_(2, 3) = 1.0;
+  matrix_a_(3, 2) = (lf_ * 2cf_ - lr_ * 2cr_) / iz_;
 
   matrix_a_coeff_ = Matrix::Zero(matrix_size, matrix_size);
-  matrix_a_coeff_(1, 1) = 0910-question;
-  matrix_a_coeff_(1, 3) = 0910-question;
-  matrix_a_coeff_(3, 1) = 0910-question;
-  matrix_a_coeff_(3, 3) = 0910-question;
+  matrix_a_coeff_(1, 1) = -(2cr_ + 2cf_) / mass_;
+  matrix_a_coeff_(1, 3) = (-2cf_ * lf_ + 2cr_ * lr_) / mass_;
+  matrix_a_coeff_(3, 1) = -(2cf_ * lf_ - 2cr_ * lr_) / iz_;
+  matrix_a_coeff_(3, 3) = -(2cf_ * lf_ * lf_ + 2cr_ * lr_ * lr_) / iz_;
 
   /*
   b = [0.0, c_f / m, 0.0, l_f * c_f / i_z]^T
